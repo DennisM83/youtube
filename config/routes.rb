c419c145982ddc99312b244e56063b91518dbbb7
Dennis Wenrich <dennisfinley@gmail.com>
2023-06-11 20:11:40 -0400

    created route for media view

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  get 'pages/media'
   devise_for :users
   resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "posts#index"
-
+  get "pages/media"
   get "posts/:id/download", to: "posts#download", as: "download_post"
 end
